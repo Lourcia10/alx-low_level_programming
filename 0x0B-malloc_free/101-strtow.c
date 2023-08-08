42ebcee628ba9aaf5a9bd6d9c134aa1bf0b01f76
Lourcia10 <blourcia@gmail.com>
2023-08-08 09:27:14 -0700

    6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,11 +9,9 @@
  */
 
 int count_word(char *s)
-
 {
 
 int flag, c, w;
-
 flag = 0;
 w = 0;
 for (c = 0; s[c] != '\0'; c++)
@@ -26,25 +24,23 @@ flag = 1;
 w++;
 }
 }
+
 return (w);
 }
 
 /**
- * **strtoq - splits a string into words
+ * **strtow - splits a string into words
  * @str: string to split
  *
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
 
-char **strtoq(char *str)
-
+char **strtow(char *str)
 {
-
 char **matrix, *tmp;
 
 int i, k = 0, len = 0, words, c = 0, start, end;
-
 while (*(str + len))
 len++;
 words = count_word(str);
@@ -75,5 +71,6 @@ else if (c++ == 0)
 start = i;
 }
 matrix[k] = NULL;
+
 return (matrix);
 }
